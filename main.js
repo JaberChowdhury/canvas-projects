777a2e5c8d6c244c8a6099096378fed66d48ecd1
black <jaberhc2002@gmail.com>
2023-12-15 17:51:36 +0600

    gravity tutorial done

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 import "./style.css";
-import Screen from "./src/Screen.js";
 import Color from "./src/Color.js";
 import dom from "./src/dom.js";
-import Perticle from "./src/Perticle.js";
 
 window.onload = () => {
   const canvas = dom("#canvas");
   const context = canvas.getContext("2d");
+
   const size = {
     w: window.innerWidth,
     h: window.innerHeight,
   };
+
   window.onresize = () => {
     size.w = window.innerWidth;
     size.h = window.innerHeight;
@@ -18,33 +18,70 @@ window.onload = () => {
     canvas.height = window.innerHeight;
   };
 
-  const limit = 1;
-  const perticleArray = [];
+  canvas.width = size.w;
+  canvas.height = size.h;
 
-  const { w, h } = size;
-  canvas.width = w;
-  canvas.height = h;
-  context.filStyle = "#000000";
-  context.fillRect(0, 0, w, h);
-  context.fill();
+  // state
+  const limit = 100;
+  const perticlesArray = [];
+  const colors = [
+    "#FDF7E4",
+    "#FAEED1",
+    "#DED0B6",
+    "#BBAB8C",
+    "#9A3B3B",
+    "#C08261",
+    "#E2C799",
+    "#F2ECBE",
+  ];
 
-  (() => {
-    for (let i = 0; i < limit; i++) {
-      perticleArray.push(new Perticle(context));
+  context.fillRect(0, 0, canvas.width, canvas.height);
+
+  class Perticle {
+    constructor() {
+      this.size = 10;
+      this.x = Math.floor(Math.random() * canvas.width - 20 + 1) + 20;
+      this.y = Math.floor(Math.random() * 200);
+      this.dy = 5;
+      this.gravity = 1;
+      this.color = colors[Math.floor(Math.random() * colors.length)];
+      this.friction = 0.9;
     }
-  })();
+    draw() {
+      context.beginPath();
+      context.fillStyle = this.color;
+      context.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+      context.fill();
+    }
+    update() {
+      this.y += this.dy;
+      if (this.y + this.size + this.dy > canvas.height) {
+        this.dy = -this.dy * this.friction;
+      } else {
+        this.dy += this.gravity;
+      }
+    }
+  }
+
+  const init = () => {
+    for (let i = 0; i < limit; i++) {
+      perticlesArray.push(new Perticle());
+    }
+  };
+  init();
+
   const handlePerticles = () => {
-    for (let i = 0; i < perticleArray.length; i++) {
-      perticleArray[i].draw();
-      perticleArray[i].update();
+    for (let i = 0; i < limit; i++) {
+      perticlesArray[i].draw();
+      perticlesArray[i].update();
     }
   };
 
   const animate = () => {
-    context.fillStyle = "rgba(0,0,0,0.1)";
-    context.fillRect(0, 0, canvas.width, canvas.height);
+    context.clearRect(0, 0, canvas.width, canvas.height);
     handlePerticles();
     requestAnimationFrame(animate);
   };
+
   animate();
 };
