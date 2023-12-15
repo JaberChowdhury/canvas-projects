13c4bd916acb8d281d64c5123aa1f97450a67e64
black <jaberhc2002@gmail.com>
2023-12-16 00:28:59 +0600

    initial commit

--- main.js
+++ main.js
@@ -30,34 +30,8 @@ window.onload = () => {
     "#E2C799",
     "#F2ECBE",
   ];
-
   context.fillRect(0, 0, canvas.width, canvas.height);
 
-  class Perticle {
-    constructor() {
-      this.size = 10;
-      this.x = Math.floor(Math.random() * canvas.width - 20 + 1) + 20;
-      this.y = Math.floor(Math.random() * 200);
-      this.dy = 5;
-      this.gravity = 1;
-      this.color = colors[Math.floor(Math.random() * colors.length)];
-      this.friction = 0.9;
-    }
-    draw() {
-      context.beginPath();
-      context.fillStyle = this.color;
-      context.arc(this.x, this.y, this.size, 0, Math.PI * 2);
-      context.fill();
-    }
-    update() {
-      this.y += this.dy;
-      if (this.y + this.size + this.dy > canvas.height) {
-        this.dy = -this.dy * this.friction;
-      } else {
-        this.dy += this.gravity;
-      }
-    }
-  }
   const init = () => {
     for (let i = 0; i < limit; i++) {
       perticlesArray.push(new Perticle());
